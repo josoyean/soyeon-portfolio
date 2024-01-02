e5a9edc524e20a601f03b8ee5268a90c238a8b0d
josoyean <dlfjswhtnals@naver.com>
2024-01-02 21:10:29 +0900

    자기소개 수정

--- src/pages/AboutContent.js
+++ src/pages/AboutContent.js
@@ -22,7 +22,7 @@ function AboutContent() {
     <div className='about-box'>
      <div className='text-box'> <span className='text'></span> <span className='cursor'>|</span></div>
      <span className='introduce'>
-안녕하세요.<br/> 1년 6개월 차 주니어 웹 프론트엔드 개발자 입니다.<br/> 나만 이해 하는 있는 코드가 아닌 남들이 보기에도 이해가 잘 되는 코드를 쓰려고 생각하고 노력합니다. <br/>근무할 때 웹 퍼블리싱 과 프론트 작업도 같이 해서 기본적으로 HTML, CSS,JS 다룰 줄 알고,<br/>
+안녕하세요.<br/> 1년 6개월 차 주니어 웹 프론트엔드 개발자 입니다.<br/> 나만 이해 하는 있는 코드가 아닌 남들이 보기에도 이해가 잘 되는 코드를 쓰려고 생각하고 노력합니다. <br/>근무할 때 웹 퍼블리싱 과 프론트 작업도 같이 해서 기본적으로 HTML, CSS,JS 경험이 있고,<br/>
 디자인도 아름다우면서 사용자의 편의성을 고려해 사용하기<br/> 편한 웹을 제작하기 위해 끊임없이 노력하는 개발자가 되겠습니다.
 </span>
 </div>
